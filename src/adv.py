5fad8cfd4da61196318ec3a2877da8915a8482ca
wjonke <wesjonke@gmail.com>
2020-03-12 19:08:31 -0400

    added room.py logic, defined room and changed some of the printed text throughout the game to make it more user friendly

--- src/adv.py
+++ src/adv.py
@@ -12,14 +12,15 @@ passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm. 
+Alas, it is a dead end and you must go back South"""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! 
+    Sadly, it has already been completely emptied by earlier adventurers. 
+    The only exit is to the south."""),
 }
 
 
@@ -65,12 +66,12 @@ print(player.current_room)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-valid_directions = ("North", "South", "East", "West")
+valid_directions = ("n", "s", "e", "w")
 
 while True:
     cmd = input("\nWhat direction will you go? ~~~> ")
     if cmd == "q":
-        print("Goodbye!")
+        print(f"Farewell {player.name}, come back soon!")
         exit(0)
     elif cmd in valid_directions:
         player.travel(cmd)
